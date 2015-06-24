6129cb1e5a4b0443d15218d8da4644d585e2d01d
Alejandro Pereira <alepereira86@gmail.com>
2015-06-24 11:35:19 -0300

    Update config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 LOGGER_FORMAT = "%(asctime)-15s %(name)s: %(message)s"
 LOG_FILENAME = './backupscript_log.out'
-DRIVE_CONFIG_PATH = '/home/alepereira/dev/inglobe/google_drive_conn/drive_config.json'
+DRIVE_CONFIG_PATH = 'drive_config.json' #Should go absolute path here
 MAIL_SUCCESS_SUBJECT = "[BACKUPSCRIPT_SUCCESS]"
 MAIL_ERROR_SUBJECT = "[BACKUPSCRIPT_ERROR]"
